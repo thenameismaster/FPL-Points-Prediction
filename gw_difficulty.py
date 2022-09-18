58f561c39fe3c9f318b46c88c4a211a72cbc33f8
Taher Master <master.taherm@gmail.com>
2022-09-17 21:19:11 -0400

    Add files via upload

---
+++ gw_difficulty.py
@@ -0,0 +1,49 @@
+"""
+List teams based on the combined difficulty of fixtures between
+GAME_WEEK_START and GAME_WEEK_END
+Lower number means easier fixtures
+"""
+
+import json
+from get_data import get_player_data, get_fixtures_data
+
+GAME_WEEK_START = 3
+GAME_WEEK_END = 8
+
+data_file = get_player_data(False)
+fixtures_file = get_fixtures_data()
+
+with open(fixtures_file) as fixtures, open(data_file) as data:
+
+    # get fixture difficulties for each team id
+    fixtures_data = json.load(fixtures)
+    data_dict = dict()
+
+    for event in fixtures_data:
+        if isinstance(event['event'], int):
+            if GAME_WEEK_START <= event['event'] <= GAME_WEEK_END:
+                if event['team_h'] in data_dict:
+                    data_dict[event['team_h']] += event['team_h_difficulty']
+                else:
+                    data_dict[event['team_h']] = event['team_h_difficulty']
+
+                if event['team_a'] in data_dict:
+                    data_dict[event['team_a']] += event['team_a_difficulty']
+                else:
+                    data_dict[event['team_a']] = event['team_a_difficulty']
+
+    # convert team ids to names
+    json_data = json.load(data)
+    team_dict = dict()
+
+    for team_id, difficulty in data_dict.items():
+        for team in json_data['teams']:
+            if team['id'] == team_id:
+                team_dict[team['name']] = difficulty
+                continue
+
+# sort by difficulty
+team_dict_sorted = {r: team_dict[r] for r in sorted(team_dict, key=team_dict.get)}
+
+for attribute, value in team_dict_sorted.items():
+    print(attribute, value)
---
+++ players_ppg.py
@@ -0,0 +1,69 @@
+"""
+List players based on their estimated return from next fixtures in range
+GAME_WEEK_START to GAME_WEEK_END
+Uses fixture difficulty rating and ppg (from either this season or last season)
+"""
+
+import json
+from get_data import get_player_data, get_fixtures_data
+
+# Data range
+GAME_WEEK_START = 5  # GW1 = 1
+GAME_WEEK_END = 7  # Inclusive GW38 = 38
+
+# 1 = GK, 2 = DEF, 3 = MID, 4 = ATT
+POSITIONS = [1, 2, 3, 4]
+EXCLUDE_TEAMS = []
+MAX_VALUE = 15
+MIN_MINUTES_PLAYED = 90
+USE_LAST_SEASON = False
+USE_FORM = False
+
+
+def get_estimated_points(player_data, fixtures_data):
+    if USE_FORM:
+        ppg = float(player_data['form'])
+    else:
+        ppg = float(player_data['points_per_game'])
+    team_id = player_data['team']
+    estimated_points = 0
+
+    for event in fixtures_data:
+        if isinstance(event['event'], int):
+            if GAME_WEEK_START <= event['event'] <= GAME_WEEK_END:
+                if event['team_h'] == team_id:
+                    match_difficulty = event['team_a_difficulty'] / event['team_h_difficulty']
+                    estimated_points += ppg * match_difficulty
+                elif event['team_a'] == team_id:
+                    match_difficulty = event['team_h_difficulty'] / event['team_a_difficulty']
+                    estimated_points += ppg * match_difficulty
+
+    return round(estimated_points)
+
+
+data_file = get_player_data(USE_LAST_SEASON)
+fixtures_file = get_fixtures_data()
+
+with open(fixtures_file) as fixtures, open(data_file) as data:
+
+    # load fixtures data
+    fixture_data = json.load(fixtures)
+
+    # load player data
+    json_data = json.load(data)
+    data_dict = dict()
+
+    for element in json_data['elements']:
+        if float(element['minutes']) > MIN_MINUTES_PLAYED and \
+                element['element_type'] in POSITIONS and \
+                element['now_cost'] <= MAX_VALUE * 10 and \
+                element['team'] not in EXCLUDE_TEAMS:
+            player_name = element['first_name'] + ' ' + element['second_name']
+            data_dict[player_name] = get_estimated_points(element, fixture_data)
+
+
+# sort and print list
+data_dict_sorted = {r: data_dict[r] for r in sorted(data_dict, key=data_dict.get)}
+
+for attribute, value in data_dict_sorted.items():
+    print(attribute, value)
